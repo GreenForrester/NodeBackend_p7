799e54fe730924b86a5b880435077925ff215f52
GreenForests <bsaadi.mail@gmail.com>
2024-11-24 09:37:02 +0500

    Added Remarks

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//Main file
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //app.useGlobalPipes(new ValidationPipe());
